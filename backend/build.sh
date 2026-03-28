51e00cb8af1621d07e11887ed0adf343c9ae5da7
Kaushik Panigrahi <kpanigrahi5602@gmail.com>
2026-03-29 01:46:28 +0530

    Fix Render Build Directory

--- backend/build.sh
+++ backend/build.sh
@@ -2,6 +2,9 @@
 # exit on error
 set -o errexit
 
+# Ensure we are in the correct directory (backend) regardless of execution path
+cd "$(dirname "$0")"
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
